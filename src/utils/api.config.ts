ae8fd0c210d885d72a8020a5a932f0c38219d959
akashmehrotra29 <akashmehrotra29@gmail.com>
2021-07-08 22:09:29 +0530

    chore: updated backend API

--- src/utils/api.config.ts
+++ src/utils/api.config.ts
@@ -1,2 +1,2 @@
 // export const API = "http://localhost:3001";
-export const API = "https://quiz-app-backend.akashmehrotra29.repl.co";
+export const API = "https://cape-quizzes-backend.akashmehrotra29.repl.co";
